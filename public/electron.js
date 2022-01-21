90b667f104c6ea0478a7673670c17198e23e9e63
akhilsiraswal <akhilsiraswal@gmail.com>
2022-01-21 21:14:57 +0530

    connected electron with react

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+};
+
+app.whenReady().then(() => createWindow());
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
